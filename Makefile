053a6fd7fc1d5e7604a2359d5a06569aba0640fe
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-03-04 16:57:27 +0100

    feat(cli): add debug command (#24)

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
-.PHONY: help
-
 help: ## List of commands
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
@@ -14,3 +12,8 @@ deploy: ## Run deployment script
 
 tx: ## Execute a transaction to the Gateway
 	npx blueprint run transaction
+
+debug: ## Outputs Gateway's debug info
+	npx blueprint run debug
+
+.PHONY: help compile test deploy tx debug
---
+++ scripts/debug.ts
@@ -0,0 +1,43 @@
+import { NetworkProvider } from '@ton/blueprint';
+import { Gateway } from '../wrappers/Gateway';
+import { formatCoin } from '../tests/utils';
+import { Address } from '@ton/core';
+
+const GATEWAY_ACCOUNT_ID_TESTNET = Address.parse(
+    '0:7a4d41496726aadb227cf4d313c95912f1fe6cc742c18ebde306ff59881d8816',
+);
+
+export async function run(provider: NetworkProvider) {
+    const isTestnet = provider.network() === 'testnet';
+
+    const address = await provider
+        .ui()
+        .inputAddress('Enter Gateway address', isTestnet ? GATEWAY_ACCOUNT_ID_TESTNET : undefined);
+
+    const gw = await provider.open(Gateway.createFromAddress(address));
+
+    const [state, balance, seqno] = await Promise.all([
+        gw.getGatewayState(),
+        gw.getBalance(),
+        gw.getSeqno(),
+    ]);
+
+    console.log('Gateway', {
+        enabled: state.depositsEnabled,
+        tss: state.tss,
+        authority: state.authority.toRawString(),
+        value_locked: `${formatCoin(state.valueLocked)} TON`,
+        balance: `${formatCoin(balance)} TON`,
+        seqno: seqno,
+    });
+
+    console.log('Explorer link:');
+    console.log(addressLink(address, isTestnet));
+}
+
+function addressLink(address: Address, isTestnet: boolean): string {
+    const raw = address.toRawString();
+    return isTestnet
+        ? `https://testnet.tonscan.org/address/${raw}`
+        : `https://tonscan.org/address/${raw}`;
+}
